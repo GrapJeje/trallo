bfab2bf0175e01c572cbd99c1ff35aedca296925
WesselBiemans <119673590+WesselBiemans@users.noreply.github.com>
2025-03-10 10:27:47 +0100

    department naar section
    department hernoemd naar section in verband met hoe de database is genoemd

--- Website/tasks/create.php
+++ Website/tasks/create.php
@@ -12,11 +12,11 @@
     <lable for="description">Beschrijving</lable>
     <textarea id="description" name="description" required></textarea>
 
-    <label for="department">Afdeling</label>
-    <select name="department" id="department">
-        <option value="department1">department1</option>
-        <option value="department2">department2</option>
-        <option value="department3">department3</option>
+    <label for="section">Afdeling</label>
+    <select name="section" id="section">
+        <option value="section1">section1</option>
+        <option value="section2">section2</option>
+        <option value="section3">section3</option>
     </select>
 
     <!-- Status is op todo bij aanmaken -->
