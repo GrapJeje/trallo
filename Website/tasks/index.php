356acdf9fb97013c5dec600ddc8ec750f3a47a81
Jason <jasonvanl2019@gmail.com>
2025-03-11 09:55:56 +0100

    Added a standard read layout system

---
+++ Website/tasks/index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Trallo</title>
+    <link rel="stylesheet" href="../public/css/main.css">
+    <link rel="stylesheet" href="../public/css/tasks/view.css">
+</head>
+<body>
+<div class="container">
+    <div class="view-container">
+
+        <div class="view-child">
+            <?php require_once 'create.php'; ?>
+        </div>
+
+        <div class="view-child">
+            <div class="view-read">
+                <h1>Taken</h1>
+                <div class="view-read-card">
+                    <p class="view-read-organisatie">Horeca</p>
+                    <div class="view-read-card-container">
+                        <p class="view-read-title">Test</p>
+                        <p class="view-read-description">Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test Test test </p>
+                        <p class="view-read-status">To Do</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
