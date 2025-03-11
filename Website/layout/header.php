8b2ba80f63e4bea822d0730490b9fd47310a464f
martijn07 <dmb0070@edu.curio.nl>
2025-03-11 10:20:18 +0100

    Add files via upload

--- Website/layout/header.php
+++ Website/layout/header.php
@@ -1,5 +1,5 @@
 <header>
-    <img src="../public/img/logo-big-v4.png" alt="Logo">
+    <img src="public/img/logo-big-v4.png" alt="Logo">
     <h1>Trallo</h1>
     <?php require_once 'auth/loginHeader.php'; ?>
 </header>
